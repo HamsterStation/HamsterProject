6aeba1dfc5df8a6957d701b8c6f93a45f958d173
HamsterStation <164139075+HamsterStation@users.noreply.github.com>
2024-04-13 19:25:22 +0800

    小组项目

---
+++ 软件工程小组项目/model/User.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.*;
+
+public class User {
+    private String userId;
+    private String username;
+    private String password;
+    private ArrayList<Account> accounts;
+
+    // Constructor, getters and setters...
+
+    public void login(String username, String password) {
+        // Implement login logic
+    }
+}
